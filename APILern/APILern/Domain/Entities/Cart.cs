7ef36ce4f6a2991394c65c664507f899e9bea034
Danila <danilamatush72@gmail.com>
2025-07-24 14:36:29 +0300

    Update Entities

--- APILern/APILern/Domain/Entities/Cart.cs
+++ APILern/APILern/Domain/Entities/Cart.cs
@@ -5,4 +5,5 @@ public class Cart
     public int Id { get; set; }
     public int ClientId { get; set; }
     public User? Client { get; set; }
+    public List<CartItem> Items { get; set; }
 }
--- APILern/APILern/Domain/Entities/Order.cs
+++ APILern/APILern/Domain/Entities/Order.cs
@@ -6,10 +6,9 @@ namespace APILern.Domain.Entities;
 public class Order
 {
     public int Id { get; set; }
-    public List<CartItem> CartItems { get; set; }
-    public int Quantity { get; set; }
     public int UserId { get; set; }
     public User? User { get; set; }
     public DateTime PaymentDate { get; set; }
     public EnumPaymentStatus Status { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
 }
