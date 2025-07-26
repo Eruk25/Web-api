c5cb3dff6183f7e1875c0df463e1aeffba9cac44
Danila <danilamatush72@gmail.com>
2025-07-26 12:31:38 +0300

    Файлы

--- APILern/APILern/obj/Debug/net9.0/APILern.AssemblyInfo.cs
+++ APILern/APILern/obj/Debug/net9.0/APILern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APILern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f4f722d4b1070a15809975cd9fa388549c7cf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("APILern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APILern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
