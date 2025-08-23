aa5440a2a4a85e2545ef2e99db99f5b94794db67
Danila <danilamatush72@gmail.com>
2025-08-23 11:07:43 +0300

    Add authorize attributes in CartsController

--- APILern/APILern/API/Controllers/CartsController.cs
+++ APILern/APILern/API/Controllers/CartsController.cs
@@ -1,5 +1,6 @@
 using APILern.Application.DTO;
 using APILern.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
@@ -12,14 +13,14 @@ public class CartsController : ControllerBase
     {
         _cartService = cartService;
     }
-
+    [Authorize]
     [HttpPost]
     public async Task<ActionResult> AddToCartAsync(int cartId, int productId, int quantity)
     {
         await _cartService.AddItemAsync(cartId, productId, quantity);
         return Ok();
     }
-
+    [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<CartResponseAdmin>> GetCartById(int id)
     {
