ef07d537c0fe79677ca9b7fc6daae7bbf8510f0e
Eruk25 <139441436+Eruk25@users.noreply.github.com>
2025-08-23 10:43:25 +0300

    Delete frontend directory

--- frontend/my-app/src/components/buttons/ProductButton.tsx
+++
@@ -1,5 +0,0 @@
-export function ProductButton(){
-    return(
-        <button type="button" className="bg-gradient-to-r from-[#667EEA] to-[#764BA2] rounded-lg text-white font-bold">Добавить в корзину</button>
-    )
-}
--- frontend/my-app/src/services/categories.tsx
+++
@@ -1,7 +0,0 @@
-import { Category } from "@/app/types";
-
-export const fetchCategories = async (): Promise<Category[]> =>{
-    const res = await fetch("http://localhost:5270/ProductCategories")
-    const data = await res.json()
-    return data;
-}
